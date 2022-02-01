9ad7c6ce608175d34dbd9af0ccc76f4209d626ae
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-02-01 09:22:15 +0100

    musician

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,12 +1,35 @@
 import { v4 as uuidv4 } from 'uuid';
 import { INSTRUMENTS } from './instruments.js';
+import { HOSTNAME, PORT } from './network.js';
+import { argv } from 'process';
 import dgram from 'dgram';
+
+// Check arguments
+if (argv.length < 3) {
+  console.log("Not enough arguments");
+  process.exit(1);
+}
+
+// Check instrument
+const instrument = argv[2];
+if (!Object.keys(INSTRUMENTS).includes(instrument)) {
+  console.log("The instrument don't exist, here are all instruments available:");
+  Object.entries(INSTRUMENTS).forEach((i) =>
+    console.log("-",i[0])
+  );
+  process.exit(1);
+}
+
+// Initialise musician
+const INTERVAL = 1000;
+const sound = INSTRUMENTS[instrument];
+const payload = {
+  uuid: uuidv4(),
+  sound: sound,
+}
 
 // UDP Socket creation
 const socket = dgram.createSocket('udp4');
 
-// arguments
-const instrument = process.argv[2];
-const sound = INSTRUMENTS[instrument];
-console.log(sound);
-console.log(uuidv4());
+
+setInterval(() => socket.send(JSON.stringify(payload), PORT, HOSTNAME), INTERVAL);
---
+++ docker/image-musician/src/network.js
@@ -0,0 +1,10 @@
+// Multicast address
+const HOSTNAME = "239.0.0.1";
+
+// Multicast port
+const PORT = 1291;
+
+export {
+  HOSTNAME,
+  PORT
+}
